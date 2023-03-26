a03d72124dfc5887317ebbdd244152295aed5543
J-S-Harris <103317213+J-S-Harris@users.noreply.github.com>
2023-03-26 23:11:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringMessenger
+ 
---
+++ SpringMessenger/src/main/java/com/messengerProject/SpringMessenger/SpringMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.messengerProject.SpringMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessengerApplication.class, args);
+	}
+
+}
